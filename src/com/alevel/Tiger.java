33ecad233e5cfc51cb7c0f31d524185a9faf8927
aviazmina <Ali210406n@>
2018-10-23 11:46:34 +0300

    Added Animals

--- src/com/alevel/Tiger.java
+++ src/com/alevel/Tiger.java
@@ -1,12 +1,20 @@
 package com.alevel;
 
+import java.util.prefs.BackingStoreException;
+
 public class Tiger extends Animal {
     @Override
-    public void talk() {
+    public void talk(){
+        try {
+            liveInJungle();
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
         System.out.println("Rrrr!");
     }
 
-    public void liveInJungle(){
+    public void liveInJungle() throws Exception {
         System.out.println("I`m in jungles");
     }
 }
